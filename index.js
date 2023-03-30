c65a29680e7c1ec4225f653a79c58f77b7995f85
Lewis Muthaura <lewislemmu10@gmail.com>
2023-03-30 23:01:07 +0300

    Background Customization

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ const themeModal = document.querySelector('.customize-theme');
 const fontSizes = document.querySelectorAll('.choose-size span');
 var root = document.querySelector(':root');
 const colorPalette = document.querySelectorAll('.choose-color span');
+const Bg1 = document.querySelector('.bg-1');
+const Bg2 = document.querySelector('.bg-2');
+const Bg3 = document.querySelector('.bg-3');
 
 
 
@@ -135,4 +138,91 @@ fontSizes.forEach(size => {
 })
 
 
-// change primary
+//remove active class from colors
+const changeActiveColorClass = () => {
+    colorPalette.forEach(colorPicker => {
+        colorPicker.classList.remove('active');
+    })
+}
+
+// change primary colors
+colorPalette.forEach(color => {
+    color.addEventListener('click', () => {
+        let primary;
+        //remove active class from colors
+        changeActiveColorClass();
+
+        if(color.classList.contains('color-1')){
+            primaryHue = 252;           
+        } else if(color.classList.contains('color-2')){
+            primaryHue = 52;
+        } else if(color.classList.contains('color-3')){
+            primaryHue = 352;
+        } else if(color.classList.contains('color-4')){
+            primaryHue = 152;
+        } else if(color.classList.contains('color-5')){
+            primaryHue = 202;
+        }
+        color.classList.add('active');
+
+        root.style.setProperty('--primary-color-hue', primaryHue);
+    })
+})
+
+
+
+
+
+
+//theme BACKGROUND values
+let lightColorLightness;
+let whiteColorLightness;
+let darkColorLightness;
+
+//changes background color
+const changeBG = () => {
+    root.style.setProperty('--light-color-lightness', lightColorLightness);
+    root.style.setProperty('--white-color-lightness', whiteColorLightness);
+    root.style.setProperty('--dark-color-lightness', darkColorLightness);
+}
+
+
+//change background color
+Bg1.addEventListener('click', () => {
+    //add active class
+    Bg1.classList.add('active');
+    //remove active classes from the others
+    Bg2.classList.add('active');
+    Bg3.classList.add('active');
+    //remove customized changes from local storage
+    window.location.reload();
+})
+
+Bg2.addEventListener('click', () => {
+    darkColorLightness = '95%';
+    whiteColorLightness = '20%';
+    lightColorLightness = '15%';
+
+    //add active class
+    Bg2.classList.add('active');
+    // remove active class from the others
+    Bg1.classList.remove('active');
+    Bg3.classList.remove('active');
+    changeBG();
+});
+
+Bg3.addEventListener('click', () => {
+    darkColorLightness = '95%';
+    whiteColorLightness = '10%';
+    lightColorLightness = '0%';
+
+    //add active class
+    Bg3.classList.add('active');
+    // remove active class from the others
+    Bg1.classList.remove('active');
+    Bg2.classList.remove('active');
+    changeBG();
+})
+
+
+//END
