fcf685a89496bd5bb464b7ad83221c5657a3afac
Lewis Muthaura <lewislemmu10@gmail.com>
2023-03-30 21:21:00 +0300

    Font Customization

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ const messageSearch = document.querySelector('#message-search');
 //THEME
 const theme = document.querySelector('#theme');
 const themeModal = document.querySelector('.customize-theme');
+const fontSizes = document.querySelectorAll('.choose-size span');
+var root = document.querySelector(':root');
+const colorPalette = document.querySelectorAll('.choose-color span');
+
 
 
 // =================  SIDEBAR  ====================
@@ -82,3 +86,53 @@ const closeThemeModal = (e) => {
 themeModal.addEventListener('click', closeThemeModal);
 
 theme.addEventListener('click', openThemeModal);
+
+
+
+
+
+
+//========================   FONTS ===========================
+// remove active class from spans or font size selectors
+const removeSizeSelector = () => {
+    fontSizes.forEach(size => {
+        size.classList.remove('active');
+    })
+}
+
+fontSizes.forEach(size => {
+    size.addEventListener('click', () => {
+        removeSizeSelector();
+        let fontSize;
+        size.classList.toggle('active'); 
+
+        if(size.classList.contains('font-size-1')){
+            fontSize = '10px';
+            root.style.setProperty('--sticky-top-left', '5.4rem');
+            root.style.setProperty('--sticky-top-right', '5.4rem');
+        } else if(size.classList.contains('font-size-2')){
+            fontSize = '13px';
+            root.style.setProperty('--sticky-top-left', '5.4rem');
+            root.style.setProperty('--sticky-top-right', '-7rem');
+        } else if(size.classList.contains('font-size-3')){
+            fontSize = '16px';
+            root.style.setProperty('--sticky-top-left', '-2rem');
+            root.style.setProperty('--sticky-top-right', '-17rem');
+        } else if(size.classList.contains('font-size-4')){
+            fontSize = '19px';
+            root.style.setProperty('--sticky-top-left', '-5rem');
+            root.style.setProperty('--sticky-top-right', '-25rem');
+        } else if(size.classList.contains('font-size-5')){
+            fontSize = '22px';
+            root.style.setProperty('--sticky-top-left', '-10rem');
+            root.style.setProperty('--sticky-top-right', '-33rem');
+        }
+
+        //change font size of the root html element
+    document.querySelector('html').style.fontSize = fontSize;
+    })
+
+})
+
+
+// change primary
