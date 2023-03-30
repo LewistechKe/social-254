7164f9c25d57bcfd410f4efad73aa97ee868a675
Lewis Muthaura <lewislemmu10@gmail.com>
2023-03-30 20:34:58 +0300

    THEME BOX

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const menuItems = document.querySelectorAll('.menu-item');
 // MESSAGES
 const messagesNotification = document.querySelector('#messages-notification');
 const messages = document.querySelector('.messages');
+const message = messages.querySelectorAll('.message');
+const messageSearch = document.querySelector('#message-search');
+
+//THEME
+const theme = document.querySelector('#theme');
+const themeModal = document.querySelector('.customize-theme');
 
 
 // =================  SIDEBAR  ====================
@@ -31,6 +37,24 @@ menuItems.forEach(item => {
 
 
 // =================  MESSAGES  ====================
+//searches chats
+const searchMessage = () => {
+    const val = messageSearch.value.toLowerCase();
+    message.forEach(user => {
+        let name = user.querySelector('h5').textContent.toLowerCase();
+        if(name.indexOf(val) != -1){
+            user.style.display = 'flex';
+        } else{
+            user.style.display = 'none';
+        }
+    })
+}
+
+
+//Search chart
+messageSearch.addEventListener('keyup', searchMessage);
+
+//highlight messages card when messages menu is clicked
 messagesNotification.addEventListener('click', () => {
     messages.style.boxShadow = '0 0 1rem var(--color-primary)';
     messagesNotification.querySelector('.notification-count').style.display = 'none';
@@ -38,3 +62,23 @@ messagesNotification.addEventListener('click', () => {
         messages.style.boxShadow = 'none';
     }, 2000);
 })
+
+
+
+// THEME DISPLAY CUSTOMIZATION
+
+//opens modal
+const openThemeModal = () => {
+     themeModal.style.display = 'grid';
+}
+
+//closes modal
+const closeThemeModal = (e) => {
+    if(e.target.classList.contains('customize-theme')){
+        themeModal.style.display ='none';
+    }
+}
+// close modal
+themeModal.addEventListener('click', closeThemeModal);
+
+theme.addEventListener('click', openThemeModal);
